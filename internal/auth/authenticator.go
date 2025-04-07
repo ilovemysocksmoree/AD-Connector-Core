b694a76223bb61264db3db11938f8a7e05b14e48
babin17 <babin.khanal@vairav.net>
2025-04-07 11:55:29 +0545

    authenticator module for username authentication

--- internal/auth/authenticator.go
+++ internal/auth/authenticator.go
@@ -1,6 +1,12 @@
 package auth
 
-import "time"
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/ilovemysocksmoree/adcore/internal/connection"
+)
 
 type cacheKey struct {
 	username string
@@ -13,4 +19,65 @@ type cacheEntry struct {
 }
 
 type Authenticator struct {
+	connManager  *connection.Manager
+	authCfg      *AuthConfig
+	sm           *SessionManager
+	cache        map[cacheKey]cacheEntry
+	mu           sync.RWMutex
+	cleanupTimer *time.Timer
+}
+
+func NewAuthenticator(cm *connection.Manager, cfg *AuthConfig) (*Authenticator, error) {
+	if cfg == nil {
+		return nil, fmt.Errorf("config is nil")
+	}
+
+	if cfg.BaseDN == "" {
+		return nil, fmt.Errorf("baseDN is empty")
+	}
+
+	if cm == nil {
+		return nil, fmt.Errorf("connection manager is nil")
+	}
+
+	sm := NewSessionManager(cfg.SessionTTL)
+	authenticator := &Authenticator{
+		connManager: cm,
+		authCfg:     cfg,
+		sm:          sm,
+		cache:       make(map[cacheKey]cacheEntry),
+	}
+
+	if cfg.CacheEnabled {
+		authenticator.startCacheCleanup()
+	}
+
+	return authenticator, nil
+}
+
+func (a *Authenticator) startCacheCleanup() {
+	a.cleanupTimer = time.AfterFunc(5*time.Minute, func() {
+		a.cleanupCache()
+		a.startCacheCleanup()
+	})
+}
+
+func (a *Authenticator) cleanupCache() {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	now := time.Now()
+	count := 0
+
+	for k, v := range a.cache {
+		if now.After(v.expiration) {
+			delete(a.cache, k)
+			count++
+		}
+	}
+
+	if count > 0 {
+		fmt.Printf("cleaned-up no.of: %d cache \n", count)
+	}
+
 }
