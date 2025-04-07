88e880e47a5407e2ba8363e56c422a009d7b9ed1
baiman <baiman@baiman.delilah>
2025-04-07 06:39:29 +0545

    connection manager setup

--- internal/connection/manager.go
+++ internal/connection/manager.go
@@ -1,3 +1,31 @@
 package connection
 
-type Manager struct{}
+import (
+	"sync"
+	"time"
+)
+
+type Manager struct {
+	pool         *ConnectionPool
+	cfg          *ConnConfig
+	statLock     sync.RWMutex
+	connStats    map[string]int
+	lastPingTime time.Time
+	isHealthy    bool
+}
+
+func GetConnectionManager(config *ConnConfig) *Manager {
+	if err := config.Validate(); err != nil {
+		return nil
+	}
+
+	pool := NewConnectionPool(config)
+	manager := &Manager{
+		pool:      pool,
+		cfg:       config,
+		isHealthy: false,
+		connStats: make(map[string]int),
+	}
+
+	return manager
+}
